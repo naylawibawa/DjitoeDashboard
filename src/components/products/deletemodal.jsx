2f53c3f195bd35c28556a3c9638dd479e67b2f0a
naylawibawa <naylawibawaa@gmail.com>
2025-09-20 11:01:08 +0700

    Created modals to detele and change the status on the product page

---
+++ src/components/products/deletemodal.jsx
@@ -0,0 +1,28 @@
+import { createPortal } from "react-dom";
+
+export default function ProductDeleteModal({ isOpen, product, onClose, onConfirm }) {
+  if (!isOpen || !product) return null;
+
+  return createPortal(
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-40 font-inter">
+      <div className="relative bg-white rounded-xl p-6 w-96 text-center shadow-lg">
+        <button className="p-1 absolute top-3 right-3 text-gray-500 hover:bg-gray-100 rounded-lg" onClick={onClose}>
+          ✕
+        </button>
+        <h2 className="text-xl font-semibold text-indigo-400 mb-4">Delete Product</h2>
+        <p className="text-gray-600 mb-6">
+          This action cannot be undone. Do you want to delete this <b>{product.product}</b> product?
+        </p>
+        <div className="flex justify-end gap-4">
+          <button className="px-6 py-1 bg-red-200 text-red-700 hover:bg-red-300 rounded-lg" onClick={onClose}>
+            No
+          </button>
+          <button className="px-6 py-1 bg-green-200 text-green-700 hover:bg-green-300 rounded-lg" onClick={onConfirm}>
+            Yes
+          </button>
+        </div>
+      </div>
+    </div>,
+    document.body
+  );
+}
